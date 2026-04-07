99ad2f4bde61f0792a3993bb1a3ecb72c04feac5
Ezhilarasan U <32504454+Ezhilarasan14@users.noreply.github.com>
2026-04-07 12:19:30 +0530

    Update test_add.py

--- tests/test_add.py
+++ tests/test_add.py
@@ -10,11 +10,14 @@ def test_add_numbers():
     assert add(-1, 1) == 0
 
 
-@pytest.mark.parametrize("a, b, expected", [
-    (1, 2, 3),
-    (10, 20, 30),
-    (-1, -1, -2),
-    (0, 0, 0),
-])
+@pytest.mark.parametrize(
+    "a, b, expected",
+    [
+        (1, 2, 3),
+        (10, 20, 30),
+        (-1, -1, -2),
+        (0, 0, 0),
+    ],
+)
 def test_add_multiple(a, b, expected):
     assert add(a, b) == expected
